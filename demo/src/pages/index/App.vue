84903a7fdb7e38ddb7375f16704e2c74d791bd08
tangshiyangla <2971882468@qq.com>
2018-07-13 19:01:04 +0800

    第一次提交

--- demo/src/pages/index/App.vue
+++ demo/src/pages/index/App.vue
@@ -1,13 +1,33 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <img :src="img">
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+import Mock from 'mockjs'
+
 export default {
-  name: 'app'
+  name: 'app',
+  data(){
+    return {
+      img:''
+    }
+  },
+  mounted(){
+    var data = Mock.mock({
+        'list|1-10': [{
+            'id|+1': 1,
+            'isOk|1' : true,
+            'name|2-33' : /\w/,
+            'color' : Mock.Random.color(),
+            'email' : Mock.Random.email()
+        }]
+    })
+    console.log(JSON.stringify(data, null, 4),Mock.Random.date('yyyy-MM-dd HH:mm:ss'));
+    this.img = Mock.Random.image('200x100', '#894FC4', '#FFF', 'png', '!');
+  }
 }
 </script>
 
